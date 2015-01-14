48772aee61baa1372cff0ae7b6a25be72cd4ac01
Waughoo <jwaugh18@live.com>
2015-01-14 17:23:15 -0500

    Trying to get it to work

---
+++ Expressions Worksheet/js/Expressions.js
@@ -0,0 +1,9 @@
+// Expressions Dog Years
+
+// Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”
+
+var Realage = 9;
+var Dogage= Realage * 7;
+
+console.log("Sparky is " + Realage + " human years, which is " + Dogage + " in dog years.");
+
