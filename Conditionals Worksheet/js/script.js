9b6e27abdaec4e98e27b42ff46b000bfb14cb5a9
Waughoo <jwaugh18@live.com>
2015-01-21 20:07:54 -0500

    Finish worksheet

---
+++ Conditionals Worksheet/js/script.js
@@ -0,0 +1,57 @@
+// Fahrenheit to Celsius Converter
+
+var Far1 = prompt("Please enter the temperature");
+var Far2 = (Far1 - 32) * 5 / 9;
+
+console.log("The temperature is " +(Far2) + " degrees in Celsius.")
+prompt("The temperature is " + Far2 + " degrees in Celsius.")
+
+
+//Check the login
+var User = prompt("Enter Username")
+
+if (User || JSmith){
+	var Pass =prompt("Enter Password");
+}else{
+	console.log("User not found. Try again.")
+	prompt("User not found. Try again.")
+	User
+}
+
+if (Pass || rocksalt){
+	alert("Welcome John Smith")
+	console.log("Welcome Mr. Smith")
+}else{
+	console.log("Password does not match our records")
+	prompt("Password does not match our records")
+	prompt("Enter Password")
+}
+
+//Movie Time
+
+
+var Time = prompt("Enter time of movie");
+var Age = prompt("Enter age of customer");
+
+
+if (Age <= 55){
+	console.log("You pay $12.00 for your ticket.")
+	alert("You pay $12.00 for your ticket.")
+}
+if (Age >= 10){
+	console.log("You pay $7.00 for your ticket.")
+	alert("You pay $7.00 for your ticket.")
+}
+if (Time >= 5){
+	console.log("You pay $7.00 for your ticket.")
+	alert("You pay $7.00 for your ticket.")
+}else if(Time <= 3){
+	console.log("You pay $7.00 for your ticket.")
+	alert("You pay $7.00 for your ticket.")
+}
+
+	
+	
+	
+	
+
