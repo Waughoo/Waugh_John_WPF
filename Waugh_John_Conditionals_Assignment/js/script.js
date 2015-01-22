5ebfdcc39f5fc20bae329bb4a649e619368ca767
Waughoo <jwaugh18@live.com>
2015-01-22 18:51:30 -0500

    Finished

--- Waugh_John_Conditionals_Assignment/js/script.js
+++ Waugh_John_Conditionals_Assignment/js/script.js
@@ -1,20 +1,25 @@
 // Conditions Assignment
 
 // code to figure out how much discount to give to employees and valued customers
-var Emp = 20;
-var Cus = 15;
+var Emp1 = .20;
+var Emp2 = 20;
+var Cus = .15;
+var Cus2 = 15;
 var Ans = "Yes";
 var Cho = prompt("Employee?");
 
 if (Cho==Ans){
-	var Pay = prompt("How much are you spending?")
-	var Disc1 = Pay - (Pay * Emp);
-	console.log("With your emoployee discount of " + Emp + "%, you only pay " + Disc1 + " for your items.")
-	alert("With your emoployee discount of " + Emp + "%, you only pay " + Disc1 + " for your items.")
+	var Pay = prompt("How much are you spending?");
+	var Disc0 = Pay * Emp1;
+	var Disc1 = Pay - Disc0;
+	console.log("With your emoployee discount of " + Emp2 + "%, you only pay $" + Disc1 + " for your items.")
+	alert("With your emoployee discount of " + Emp2 + "%, you only pay $" + Disc1 + " for your items.")
 }else if (Cho=="No"){
 	var Du = prompt("Customer?");
-	var Disc2 = Pay - (Pay * Cus);
-	console.log("With your emoployee discount of " + Cus + "%, you only pay " + Disc2 + " for your items.")
-	alert("With your emoployee discount of " + Cus + "%, you only pay " + Disc2 + " for your items.")
+	var Pay = prompt("How much are you spending?");
+	var Disc3 = Pay * Cus;
+	var Disc2 = Pay - Disc3;
+	console.log("With your customer discount of " + Cus2 + "%, you only pay $" + Disc2 + " for your items.")
+	alert("With your customer discount of " + Cus2 + "%, you only pay $" + Disc2 + " for your items.")
 }
 
