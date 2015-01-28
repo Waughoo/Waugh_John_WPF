11e8da128b70dc5ed71a1757bb19807e7e3ce551
Waughoo <jwaugh18@live.com>
2015-01-28 18:01:21 -0500

    Added new folder

---
+++ Functions Worksheet/js/script.js
@@ -0,0 +1,30 @@
+//Functions Worksheet
+
+//Circumference
+//Calculate the circumference of a circle
+
+var pi = Math.PI
+var rad = 12;
+var cir = (pi + rad);
+
+console.log("The circumfrence of the circle is " + cir + ".")
+
+function calcCir(pi, rad){
+	return 2(cir)
+}
+
+
+//Stung
+
+//It takes 8.666666667 bee stings per pound to kill an animal
+//Calculate how many bee stings are needed to kill an animal in a function 
+
+var weight = 250;
+var sting = 8.666666667;
+var stung = weight * sting;
+
+console.log("It takes " + stung + " bee stings to kill this animal.")
+
+function stung(w, s){
+	return w * s
+}
