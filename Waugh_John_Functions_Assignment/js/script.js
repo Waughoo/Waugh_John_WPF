d1251cad70973fa320957e7942ffc27dd9f5ef91
Waughoo <jwaugh18@live.com>
2015-01-29 19:12:16 -0500

    Figured out a anonymous function
    Yay!!!

---
+++ Waugh_John_Functions_Assignment/js/script.js
@@ -0,0 +1,26 @@
+// Functions Assignment
+
+// Trying to figure out how many pounds of food my children eat in a week.
+
+var Kids =7; //Number of kids I have
+var Variety = 15; //How many different kinds of food there are
+var Food = 1.3333333333; //How much each food item ways.
+var Type = (Variety * Food); //Fomula for figuring out the amount
+var CalcPounds = Kids * Type; //Final formlua
+console.log("My " + Kids + " kids eat a combined total of " + CalcPounds + "lbs. of food each week. At this rate, they will eat me out of house and home.")// If you had seven kids, you would feel the same way
+
+function CalcPounds(K, T){
+	return Pounds
+}
+
+// Trying to do a anonymous function
+
+// I am going to try an anonymous function
+
+var calcCube = function(length, width, height){//let's try some math
+	var cube = length * width * height;// formula for a cube
+	return cube
+}
+var CalcCube = calcCube(25, 15, 43);//invoking
+
+console.log("The area of the cube is " + CalcCube)
